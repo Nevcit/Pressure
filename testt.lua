f68d333756c3195b69cb26fda160cdf895880652
Nevcit <160295302+Nevcit@users.noreply.github.com>
2024-08-13 12:13:04 +0700

    Add files via upload

---
+++ testt.lua
@@ -0,0 +1,52 @@
+getgenv().SecureMode = true
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+   Name = "GOA HUB",
+   LoadingTitle = "Pressure",
+   LoadingSubtitle = "by Nevcit",
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "Big Hub"
+   },
+   Discord = {
+      Enabled = false,
+      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+   KeySystem = false, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Untitled",
+      Subtitle = "Key System",
+      Note = "No method of obtaining the key is provided",
+      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
+})
+
+local Tab = Window:CreateTab("Tab Example", 4483362458) -- Title, Image
+
+local Button = Tab:CreateButton({
+   Name = "New Version",
+   Callback = function()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/Nevcit/Pressure/main/GOA_HUB_NEW_VERSION"))
+   end,
+})
+
+local Button = Tab:CreateButton({
+   Name = "Old Version ( No Update )",
+   Callback = function()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/Nevcit/Pressure/main/GOA_HUB_OLD_VERSION"))
+   end,
+})
+
+local Button = Tab:CreateButton({
+   Name = "If you found bug, comment on this link",
+   Callback = function()
+      setclipboard("https://scriptblox.com/script/Pressure-GOA-HUB-16519")
+   end,
+})
+
